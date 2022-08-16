c23face81321ce327b9c7c4eda38a109f38cb8e5
mattand <mjande@umich.edu>
2022-08-16 11:44:21 -0400

    Add kitten model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :kittens
+  get 'kittens/all'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
