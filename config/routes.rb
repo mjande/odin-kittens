bca354e1587ed231aa8472c31458e32a21acf2ab
mattand <mjande@umich.edu>
2022-08-16 11:45:13 -0400

    Add default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resources :kittens
-  get 'kittens/all'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'kittens#index'
 end
